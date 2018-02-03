27489c5b287d189fbffbec1329ded058e4dbe773
mithun.s <mithuns239@gmail.com>
2018-02-03 23:40:23 +0530

    2nd finish and passport added

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express=require('express');
 const authRoutes=require('./routes/auth.route');
 const expressLayout=require("express-ejs-layouts");
 const path = require('path');
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app=express();
 const port=3000;
 
-app.use(express.static(__dirname + '/styles'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/images'));
+
 app.use(expressLayout);
 app.use(bodyParser());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use('/auth',authRoutes);
 
 
@@ -19,35 +21,21 @@ app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 
-
-// app.get('/',(req,res)=>{
-//     res.render('layout');
-// });
-
-// app.get('/about', function(req, res) {
-//     res.render('about', { layout: "layout"}); 
-//    });
-
-// app.get('/about',(req,res)=>{
-//     debugger
-//     res.render('about');
-// });
-
 app.get('/', function(req, res, next) {
     res.render('home', { title: 'Express template test ' , layout: "layout.ejs"}); // Specify layout file for different page (e.g about-us , dashboard )
    });
 
-app.get('/about', function(req, res, next) {
-    res.render('about', { title: 'Express template test ' , layout: "layout.ejs"}); // Specify layout file for different page (e.g about-us , dashboard )
-   });
+// app.get('/about', function(req, res, next) {
+//     res.render('about', { title: 'Express template test ' , layout: "layout.ejs"}); // Specify layout file for different page (e.g about-us , dashboard )
+//    });
 
-   app.get('/pricing', function(req, res, next) {
-    res.render('pricing', { title: 'Express template test ' , layout: "layout.ejs"}); // Specify layout file for different page (e.g about-us , dashboard )
-   });
+//    app.get('auth/login', function(req, res, next) {
+//     res.render('login', { title: 'Express template test ' , layout: "layout.ejs"}); // Specify layout file for different page (e.g about-us , dashboard )
+//    });
 
-app.get('/users', function(req, res, next) {
-    res.render('users', { title: 'Express template test ' , layout: "layout.ejs"}); // Specify layout file for different page (e.g about-us , dashboard )
-   });
+// app.get('/users', function(req, res, next) {
+//     res.render('users', { title: 'Express template test ' , layout: "layout.ejs"}); // Specify layout file for different page (e.g about-us , dashboard )
+//    });
 
 app.listen(port,()=>{
     console.log('App listening on ',port);
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,20 +1,11 @@
 const router=require('express').Router();
 
 
-//auth login
 
-router.get('/login',(req,res)=>{
-    res.render('login');
-});
-
-router.get('/logout',(req,res)=>{
-    res.send('logging out...');
-})
-
-router.get('/google',(req,res)=>{
-    res.render('login');
-});
-
+        router.get('/login', function(req, res, next) {
+            console.log(__dirname);
+         res.render('login', { title: 'Express template test ' , layout: "../views/layout.ejs"}); // Specify layout file for different page (e.g about-us , dashboard )
+        });
 
 
 module.exports=router;
